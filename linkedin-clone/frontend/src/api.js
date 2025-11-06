d00105bb1dd91301a8fb174ea91f6cfd38cbea65
venkadesh1999-M <messivenkadesh135@gmail.com>
2025-11-06 06:49:30 +0530

    Final Fix: Added /api back to Frontend Base URL to resolve 404

--- linkedin-clone/frontend/src/api.js
+++ linkedin-clone/frontend/src/api.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 const base = process.env.REACT_APP_API_BASE || 'https://social-media-website-vt6g.onrender.com/api';
 export default axios.create({ baseURL: base });
+
+
+
